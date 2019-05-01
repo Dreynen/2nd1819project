855e7ed308ebde4e93ad4e93008f278e74d0707e
Jared Hechanova <jlhechanova@gmail.com>
2019-05-01 15:30:58 +0000

    edit to .c file

--- hechanova_project.c
+++ hechanova_project.c
@@ -29,19 +29,15 @@ int main(void)
 /*
 	more structured user input methods
 	validate date and time
-	check if same date departure and arrival date
-	if same, no need to validate departure and arrival time
+	if flight has same departure and arrival date, validate times.
 	validate departure and arrival between two flights
-		if same day, compare time
-			departure must come after arrival
-			or arrival must come before departure
-			(before or after)
+		if same day, validate time
+			departure time after arrival time or arrival time before departure time
 		else, compare date
-			departure must come after arrival
-			or arrival must come before departure
-			(before or after pa rin)
+			departure date after arrival date or arrival date before departure date
+	how to keep things sorted even after editing????
 	use flight IDs and passport numbers as keys
-	how to properly reference flights from passenger??
+	how to properly cross-reference flights and passengers??
 	spam user prompts? maybe later
 
 */
