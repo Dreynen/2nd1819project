130bcdb9de67359cf5bff1813b22e9780dcdc1f6
Jared Hechanova <jlhechanova@gmail.com>
2019-05-12 09:55:37 +0000

    added all functionalities except editing

--- hechanova_project.h
+++ hechanova_project.h
@@ -1,5 +1,23 @@
 #include "project_lib.h"
 
+void RunApplication()
+{
+    F_info *flights = NULL;
+    P_info *passengers = NULL;
+    int flight_count = 0, passenger_count = 0;
+
+    load(&flights, &passengers, &flight_count, &passenger_count);
+
+    do
+    {
+        menu_out();
+    }
+    while(switchboard(&flights, &passengers, &flight_count, &passenger_count));
+
+    save(flights, passengers, flight_count, passenger_count);
+    deallocate(&flights, &passengers);
+}
+
 void menu_out()
 {
 	printf("[1] Add A Flight\n");
